a6726a4d567c933bf3f719eaeef3e53fe61f2ad3
boyana rahul <boyana.rahul2021@vitstudent.ac.in>
2024-06-07 10:45:56 +0530

    java student to kt

---
+++ app/src/main/java/com/example/rahul1/kotlinexs/student.kt
@@ -0,0 +1,7 @@
+package com.example.rahul1.kotlinexs
+
+
+/**
+ * POJO - plain old java object / VO value object / model class -- box with partitionns
+ */
+class Student(@JvmField var name: String, var age: Int, var postalAddress: String)
