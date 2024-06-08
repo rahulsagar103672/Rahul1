4e5ac7869ca1eba54dfb215462bd3b8f25d91cf7
boyana rahul <boyana.rahul2021@vitstudent.ac.in>
2024-06-08 10:59:19 +0530

    button click log

--- app/src/main/java/com/example/rahul1/MainActivity.kt
+++ app/src/main/java/com/example/rahul1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.rahul1
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+    }
+
+    fun clickHandler(view: View) {
+        Log.i("MainActivity-clickhandler","button clicked")
     }
 }
