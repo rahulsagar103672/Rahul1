04e58efcc9e2f9d7030a6d5e3d78d3c6599ea0e2
boyana rahul <boyana.rahul2021@vitstudent.ac.in>
2024-06-14 10:39:13 +0530

    call stack, step into, step out, eval expr

--- app/src/main/java/com/example/rahul1/MainActivity.kt
+++ app/src/main/java/com/example/rahul1/MainActivity.kt
@@ -62,13 +62,35 @@ class MainActivity : AppCompatActivity() {
 
     fun clickHandler(view: View) {
         Log.i("MainActivity-clickhandler","button clicked")
-        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
-        startActivity(dialIntent)
+        /*var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+        startActivity(dialIntent)*/
+
+
+        var c = 10 + 20
+        add(10,20)
 
         var hIntent:Intent = Intent(this,HomeActivity::class.java)
         hIntent.putExtra("mykey","android-vit-rahul")
-        throw NullPointerException("demo vit exception")
+        //throw NullPointerException("demo vit exception")
         startActivity(hIntent)
+    }
+
+    private fun add(i: Int, i1: Int):Int {
+        var c = 20
+        var d = 20 * i
+        mul(5,4)
+        return i +i1
+
+    }
+
+    private fun mul(i: Int, i1: Int) {
+        val d = i * i1
+        div(6,3)
+
+    }
+
+    private fun div(i: Int, i1: Int) {
+        add(10,20)
     }
 
     fun inflateXml(){
